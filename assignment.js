ff3c72c25d1192c1d5d10737a45a88d26e11b5c4
Faisal Odunuga <faisalodunuga@gmail.com>
2025-04-21 19:53:51 +0100

    8-10

--- assignment.js
+++ assignment.js
@@ -1,7 +1,6 @@
 /*
 1. Monthly Budget Calculator: Given an array of your monthly expense amounts, use a loop to compute the total expense and then calculate the average.
 */
-
 const expenses = [200, 400, 700, 20, 190, 2000];
 let sum = 0;
 let average;
@@ -10,10 +9,8 @@ for (let i = 0; i < expenses.length; i++) {
   average = sum / expenses.length;
 }
 console.log(average);
-200 + 400 + 700 + 20 + 190 + 2000;
 
 // 2.Temperature Analyzer: With an array of daily temperatures (in °C), find the highest and lowest temperatures by iterating once through the array.
-
 let temps = ["7°C", "20°C", "-7°C", "30°C", "52°C"];
 let smallest = temps[0];
 let biggest = temps[0];
@@ -29,7 +26,6 @@ for (let i = 0; i < temps.length; i++) {
 console.log(smallest, biggest);
 
 // 3. Discounted Prices Generator: Given an array of product prices and a fixed discount rate (e.g. 10%), loop through the prices to build a new array of discounted prices.
-
 const prices = [200, 400, 700, 20, 190, 2000];
 let discountedPrices = [];
 for (let i = 0; i < prices.length; i++) {
@@ -38,7 +34,6 @@ for (let i = 0; i < prices.length; i++) {
 console.log(discountedPrices);
 
 // 4. Counting Age Groups: From an array of people’s ages, count how many are minors (under 18) and how many are adults (18 and over).
-
 const ages = [20, 40, 10, 15, 2, 18, 100, 2, 25, 34];
 let minors = 0;
 let adults = 0;
@@ -52,7 +47,6 @@ for (let i = 0; i < ages.length; i++) {
 console.log("We have " + minors + " minors and " + adults + " adults.");
 
 // 5. High‑Spending Transactions:Given an array of amounts, find and collect all amounts above a certain threshold (e.g. N1000) into a new array.
-
 const prices2 = [200, 400, 700, 20, 190, 2000];
 let overSpent = [];
 for (let i = 0; i < prices.length; i++) {
@@ -83,5 +77,46 @@ for (let i = 0; i < list.length; i++) {
     targetItemIndex = i;
   }
 }
-
 console.log(targetItemIndex);
+
+// 8. Reverse an Array In‑Place: Without using the built‑in method, swap elements in the array via a loop to reverse it manually.
+const list2 = ["Snacks", "Food", "Drinks", "MacBook", "Cream", "Lambo"];
+for (let i = 0; i < list2.length / 2; i++) {
+  let first = list2[i];
+  let last = list2[list2.length - 1 - i];
+  list2[i] = last;
+  list2[list2.length - 1 - i] = first;
+}
+console.log(list2);
+
+// 9. Duplicate Removal: From an array that may contain duplicate entries (e.g. attendee names), build a new array of uniques by checking witha loop and using indexOf to avoid repeats.
+
+const names = [
+  "Fuad",
+  "Faisal",
+  "Fareed",
+  "Furqan",
+  "Farhan",
+  "faRhan",
+  "FURQAN",
+];
+let uniqueNames = [];
+for (let i = 0; i < names.length; i++) {
+  let currentName = names[i].toLowerCase();
+  if (uniqueNames.indexOf(currentName) === -1) {
+    uniqueNames.push(currentName);
+  }
+}
+console.log(uniqueNames);
+
+// 10. Word‑Counting in a Sentence: Split a sentence into words (using String.split), then loop through the resulting array to count how many words start with a vowel.
+let sentence = "This is a sentence and i am gonna loop through it".split(" ");
+let vowelCount = 0;
+for (let i = 0; i < sentence.length; i++) {
+  let currWord = sentence[i];
+
+  if ("aeiouAEIOU".includes(currWord[0])) {
+    vowelCount++;
+  }
+}
+console.log(vowelCount);
